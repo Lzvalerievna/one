5905dadcdc51930ddd376243190a96c397cf5888
Lilia <lzvalerievna@gmail.com>
2021-01-07 15:22:08 +0300

    Исправления

--- src/js/index.js
+++ src/js/index.js
@@ -34,20 +34,27 @@ mobileSlider();
 window.addEventListener('resize', () => {
   mobileSlider();
 });
-
+export let bodyNoScroll = document.querySelector('.page-body');
 export let btn = document.querySelector('.page-sidebar');
 export let openBtn = document.querySelector('.menu-icon-burger');
 export let closeBtn = document.querySelector('.header-icon-close');
 export let closeSidebar = document.querySelector('.page-sidebar__overlay');
 openBtn.addEventListener('click',function () {
-    btn.classList.add('page-sidebar_active');
+  btn.classList.add('page-sidebar_active');
+  bodyNoScroll.classList.add('page-body_active');
+})
+openBtn.addEventListener('click',function () {
+  btn.classList.add('page-sidebar_active');
+  bodyNoScroll.classList.add('page-body_active');
 })
 closeBtn.addEventListener('click',function () {
   btn.classList.remove('page-sidebar_active');
+  bodyNoScroll.classList.remove('page-body_active');
 })
 
 closeSidebar.addEventListener('click',function () {
   btn.classList.remove('page-sidebar_active');
+  bodyNoScroll.classList.remove('page-body_active');
 })
 
 export let modal = document.querySelector('.modal-connection');
@@ -58,31 +65,41 @@ export let closeModalFon = document.querySelector('.modal-connection__overlay');
 
 openModal.addEventListener('click',function () {
   modal.classList.add('modal-connection_active');
+  bodyNoScroll.classList.add('page-body_active');
 })
 openModallConection.addEventListener('click',function () {
   modal.classList.add('modal-connection_active');
+  bodyNoScroll.classList.add('page-body_active');
 })
 closeModal.addEventListener('click',function () {
   modal.classList.remove('modal-connection_active');
+  bodyNoScroll.classList.remove('page-body_active');
 })
 closeModalFon.addEventListener('click',function () {
   modal.classList.remove('modal-connection_active');
+  bodyNoScroll.classList.remove('page-body_active');
 })
 
 export let modalCall = document.querySelector('.modal-call');
-export let openModalCall = document.querySelector('.menu-icon-call','.footer-icon-call');
+export let openModalCall = document.querySelector('.menu-icon-call');
 export let openModalCallOne = document.querySelector('.footer-icon-call');
 export let closeModalCall = document.querySelector('.button-modal-close-icon');
 export let closeModalFonCall = document.querySelector('.modal-call__overlay');
 openModalCall.addEventListener('click',function () {
   modalCall.classList.add('modal-call_active');
+  bodyNoScroll.classList.add('page-body_active');
 })
 openModalCallOne.addEventListener('click',function () {
   modalCall.classList.add('modal-call_active');
+  bodyNoScroll.classList.add('page-body_active');
 })
 closeModalCall.addEventListener('click',function () {
   modalCall.classList.remove('modal-call_active');
+  bodyNoScroll.classList.remove('page-body_active');
 })
 closeModalFonCall.addEventListener('click',function () {
   modalCall.classList.remove('modal-call_active');
+  bodyNoScroll.classList.remove('page-body_active');
 })
+
+
